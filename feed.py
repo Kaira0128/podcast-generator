e5837bc663b4172f9b3c608951e4542b893fd74d
Angie Daniela Soto Picado <111537076+Kaira0128@users.noreply.github.com>
2025-03-08 19:16:39 -0600

    Update feed.py

--- feed.py
+++ feed.py
@@ -1,39 +1,42 @@
-title: The Future in Tech
-subtitle: Powered by LinkedIn Learning
-author: Ray Villalobos
-description: Conversations with leaders building next generation techology tools.
-image: /images/artwork.jpg
-language: en-us
-category: Technology
-format: audio/mpeg
-item:
-  - title: EP01-What's Happening in 2023
-    description: Ray Villalobos and Content Managers Natalie Pao and Simon St. Laurent discuss what 2023 holds for the future in technology.
-    published: Thu, 12 Jan 2023 18:00:00 GMT
-    file: /audio/TFIT01.mp3
-    duration: 00:00:36
-    length: 576,324
-  - title: EP02-What is Generative AI?
-    description:  Pinar Demirdag discusses the fundamentals of generative AI, its practical applications, and future developments as companies create AI strategies in 2023.
-    published: Thu, 19 Jan 2023 18:00:00 GMT
-    file: /audio/TFIT02.mp3
-    duration: 00:00:22
-    length: 351,963
-  - title: EP03-How Generative AI Will Change DevOps
-    description:  Jerome Hardaway explores how generative AI can revolutionize the software development process by increasing efficiency, automating repetitive tasks, and creating more sophisticated and innovative solutions.
-    published: Thu, 26 Jan 2023 18:00:00 GMT
-    file: /audio/TFIT03.mp3
-    duration: 00:00:19
-    length: 305,488
-  - title: EP04-Building Responsible AI Systems
-    description:   Ayodele Odubela, a Data Scientist, Machine Learning professional and AI Entrepreneur, discusses the ethical issues surrounding the rapid adoption of AI systems, including transparency, minimizing bias, ensuring fairness, and potential ramifications around privacy, security, and governance.
-    published: Thu, 2 Feb 2023 18:00:00 GMT
-    file: /audio/TFIT04.mp3
-    duration: 00:00:29
-    length: 470,668
-  - title: EP05-Crafting an AI Strategy For Your Business
-    description: Andreas Welsch joins this discussion to share insights on crafting an AI strategy for businesses that considers evolving technology, ethics, and legal implications, drawing from his extensive experience in managing stakeholder relationships and leading AI projects across Fortune 500 companies.
-    published: Thu, 9 Feb 2023 18:00:00 GMT
-    file: /audio/TFIT05.mp3
-    duration: 00:00:22
-    length: 352,587
+import yaml
+import xml.etree.ElementTree as xml_tree
+
+with open("feed.yaml", "r") as file:
+    yaml_data = yaml.safe_load(file)
+
+    rss_element = xml_tree.Element("rss", {"version":"2.0", 
+     "xmlns:itunes":"http://www.itunes.com/dtds/podcast-1.0.dtd", 
+     "xmlns:content":"http://purl.org/rss/1.0/modules/content/"})
+
+channel_element = xml_tree.SubElement(rss_element, "channel")
+
+link_prefix = yaml_data["link"]
+
+xml_tree.SubElement(channel_element, "title").text = yaml_data["title"]
+xml_tree.SubElement(channel_element, "format").text = yaml_data["format"]
+xml_tree.SubElement(channel_element, "subtitle").text = yaml_data["subtitle"]
+xml_tree.SubElement(channel_element, "itunes:author").text = yaml_data["author"]
+xml_tree.SubElement(channel_element, "description").text = yaml_data["description"]
+xml_tree.SubElement(channel_element, "itunes:image", {"href": link_prefix + yaml_data["image"]})
+xml_tree.SubElement(channel_element, "language").text = yaml_data["language"]
+xml_tree.SubElement(channel_element, "link").text = link_prefix
+
+xml_tree.SubElement(channel_element, "itunes:category", {"text": yaml_data["category"]})
+
+for item in yaml_data["item"]:
+    item_element = xml_tree.SubElement(channel_element, "item")
+    xml_tree.SubElement(item_element, "title"). text = item["title"]
+    xml_tree.SubElement(item_element, "itunes:author"). text = yaml_data["title"]
+    xml_tree.SubElement(item_element, "description"). text = item["description"]
+    xml_tree.SubElement(item_element, "itunes:duration"). text = item["duration"]
+    xml_tree.SubElement(item_element, "pubDate"). text = item["published"]
+
+    enclosure =xml_tree.SubElement(item_element, "enclosure", {
+        "url": link_prefix + item ["file"],
+        "type": "audio/mpeg",
+        "length": item ["length"]
+    })
+
+
+output_tree = xml_tree.ElementTree(rss_element)
+output_tree.write("podcast.xml", encoding="Utf-8",xml_declaration=True)
